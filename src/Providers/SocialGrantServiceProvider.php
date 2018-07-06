2573e96d09ba5d0e6f0dbcfacea411ef7b007fa4
Ilya Sakovich <me@hivokas.com>
2018-07-06 14:57:26 +0300

    Created service provider that enables social grant for authorization server.

---
+++ src/Providers/SocialGrantServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Hivokas\LaravelPassportSocialGrant\Providers;
+
+use Laravel\Passport\Passport;
+use Illuminate\Support\ServiceProvider;
+use League\OAuth2\Server\AuthorizationServer;
+use Hivokas\LaravelPassportSocialGrant\SocialGrant;
+use League\OAuth2\Server\Repositories\RepositoryInterface;
+use Hivokas\LaravelPassportSocialGrant\Resolvers\SocialUserResolverInterface;
+
+class SocialGrantServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->resolving(AuthorizationServer::class, function (AuthorizationServer $server) {
+            $server->enableGrantType(
+                $this->makeSocialGrant()
+            );
+        });
+    }
+
+    /**
+     * Create and configure a Social grant instance.
+     *
+     * @return SocialGrant
+     */
+    protected function makeSocialGrant(): SocialGrant
+    {
+        $grant = new SocialGrant(
+            $this->app->make(SocialUserResolverInterface::class),
+            $this->app->make(RepositoryInterface::class)
+        );
+
+        $grant->setRefreshTokenTTL(Passport::refreshTokensExpireIn());
+
+        return $grant;
+    }
+}
