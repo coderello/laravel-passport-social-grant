b8ba9a0ce126102214ec46c541243f41c49a1a8f
Ilya Sakovich <me@hivokas.com>
2018-07-06 14:42:07 +0300

    Created SocialUserResolverInterface interface.

---
+++ src/Resolvers/SocialUserResolverInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hivokas\LaravelPassportSocialGrant\Resolvers;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+
+interface SocialUserResolverInterface
+{
+    /**
+     * Resolve user by provider credentials.
+     *
+     * @param string $provider
+     * @param string $accessToken
+     *
+     * @return Authenticatable|null
+     */
+    public function resolveUserByProviderCredentials(string $provider, string $accessToken): ?Authenticatable;
+}
